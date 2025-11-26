f51e259d79ba1a6ede2be31d1c18fbe39232c403
MandyLima <161380181+MandyLima@users.noreply.github.com>
2025-11-25 21:10:45 -0300

    feat: ADD items for styling the Home

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pagina(models.Model):
+    nome_do_site = models.CharField(max_length=100)
+    logo_do_site = models.ImageField(upload_to='site/', blank=True, null=True)
+    texto_chamada = models.CharField(max_length=200, help_text="Texto principal do banner")
+    texto_sobre = models.TextField()
+    imagem_sobre = models.ImageField(upload_to='site/', blank=True, null=True)
+    endereco = models.CharField(max_length=255)
+    email = models.EmailField()
+    whatsapp = models.CharField(max_length=20)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome_do_site
+
+    class Meta:
+        verbose_name = "Configuração da Página"
+        verbose_name_plural = "Configurações da Página"
